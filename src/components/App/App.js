ae5a63bcdc5169868761be2c9ea1e4b5d74ce27c
Tiago Gomes <t.dfgomes@icloud.com>
2020-10-20 15:05:05 +0100

    feat: seting up the game area

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.table} />
+      <div className={styles.background} />
+    </div>
+  );
+}
+
+export default App;
