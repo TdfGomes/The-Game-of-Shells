36202073d7c38ff5be42c28378a6d99d2caabbb4
Tiago Gomes <t.dfgomes@icloud.com>
2020-10-22 03:41:25 +0100

    feat: add comments

--- src/utils/MathUtils.js
+++ src/utils/MathUtils.js
@@ -1,9 +1,17 @@
-const addOffset = (offSet, elementWidth) => {
-  return Math.ceil(offSet + 200 - Math.ceil(elementWidth * 4));
+/**
+ * This functions aims to center the ball with the cup.
+ * Note: We are using the 200 from the horizontal container padding
+ *
+ * @param {integer} offSetX
+ * @param {integer} elementWidth
+ */
+
+const addOffset = (offSetX, elementWidth) => {
+  return Math.ceil(offSetX + 200 - Math.ceil(elementWidth * 4));
 };
 
-const minusOffset = (offSet, elementWidth) => {
-  return Math.ceil(offSet - 200 + Math.ceil(elementWidth / 4));
+const minusOffset = (offSetX, elementWidth) => {
+  return Math.ceil(offSetX - 200 + Math.ceil(elementWidth / 4));
 };
 
 export default { addOffset, minusOffset };
--- src/utils/generateRandomInt.js
+++ src/utils/generateRandomInt.js
@@ -1,3 +1,8 @@
+/**
+ * Randomize a set of numbers bettween 0 and max (exclusive)
+ *
+ * @param {integer} max
+ */
 export default function getRandomInt(max = 3) {
   return Math.floor(Math.random() * max);
 }
